6bbb52e97622e26f03099e1ab2d80f842bfab225
Heurtault Aubin <aub.heurtault@gmail.com>
2022-11-22 22:46:27 +0100

    list

--- src/components/setup.vue
+++ src/components/setup.vue
@@ -2,7 +2,11 @@
     <div>
         <h3 class="font-extrabold">Setup</h3>
         <div>
-            <p>Each player chooses a main pawn and places it on the start tile of the scoring track. Make two draw piles with the cards next to the board, one with the Level 1 and one with the Level 2. Each player takes an answer card and an answer pawn. </p>
+            <ul class="list-disc	px-5">
+                <li><p>Each player chooses a main pawn and places it on the start tile of the scoring track.</p></li>
+                <li><p>Make two draw piles with the cards next to the board, one with the Level 1 and one with the Level 2.</p></li>
+                <li><p>Each player takes an answer card and an answer pawn.</p></li>
+            </ul>
         </div>
     </div>
 </template>
