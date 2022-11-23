60bdf26aef3f0909e00351df2972fd9dc21857de
Heurtault Aubin <aub.heurtault@gmail.com>
2022-11-23 14:59:46 +0100

    footer

--- src/components/Gameplay.vue
+++ src/components/Gameplay.vue
@@ -4,7 +4,7 @@
         <div>
             <ul class="list-disc px-5">
                 <li>The youngest player start the game</li>
-                <li>First player chooses a card level 1 or level 2 and places it on the borad in the card location</li>
+                <li>First player chooses a card level 1 or level 2 and places it on the board in the card location</li>
                 <li>Every player will now have to guess the art movement of the art piece on the card</li>
                 <li>For response each player places their answer pawn in the hole of answer card this art movement without showing it to the others</li>
                 <li>When everybody is ready, the player that chose the card flips it and reveals the answer. </li>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,8 @@
       </div>
 
     </div>
-    <footer class="relative h-64 m-2 overflow-hidden bg-indigo-500 rounded-lg">
-      <img src="/img/logos/plateau.png" class=""/>
+    <footer class="relative  m-2 overflow-hidden rounded-lg">
+      <img src="/img/logos/footer.png" class=""/>
     </footer>
   </div>
 </template>
