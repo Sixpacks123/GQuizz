d3337036dc8b5810d464222ac483643968f2d119
Heurtault Aubin <aub.heurtault@gmail.com>
2022-11-23 10:29:53 +0100

    add

--- src/components/endOfGame.vue
+++ src/components/endOfGame.vue
@@ -2,7 +2,7 @@
     <div>
         <div class=" flex flex-row items-center  ">
             <h3 class="font-extrabold ">How to Win ?</h3>
-        <img src="/img/logos/award.png"  class="w-8 h-8 animate-bounce	" alt="">
+        <img src="/img/logos/award.png"  class="w-8 h-8 p-1 animate-bounce	" alt="">
         </div>
         
         <div>
--- src/components/setup.vue
+++ src/components/setup.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <h3 class="font-extrabold">Setup</h3>
+        <div class="flex flex-row items-center">
+            <h3 class="font-extrabold">Setup</h3>
+            <img src="/img/logos/gear.png"  class="w-8 h-8 p-1 animate-spin	" alt="">
+        </div>
+
+
         <div>
-            <ul class="list-disc	px-5">
+            <ul class="list-disc px-5">
                 <li><p>Each player chooses a main pawn and places it on the start tile of the scoring track.</p></li>
                 <li><p>Make two draw piles with the cards next to the board, one with the Level 1 and one with the Level 2.</p></li>
                 <li><p>Each player takes an answer card and an answer pawn.</p></li>
