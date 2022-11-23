2fa13769ed1dcee7e0e8dd8b642134497ccd8506
Heurtault Aubin <aub.heurtault@gmail.com>
2022-11-23 14:44:08 +0100

    update

--- src/components/gameComponents.vue
+++ src/components/gameComponents.vue
@@ -3,7 +3,7 @@
         <h3 class="font-extrabold">Game Components</h3>
         <div v-for="item in items" :key="item.message">
         <div class="flex flex-row pb-4 items-center ">
-            <img :src="`${item.image}`" class="w-16 h-12 mr-5" />
+            <img :src="`${item.image}`" class="mr-5 object-fill w-10" />
             <p>{{ item.message }}</p>
         </div>   
         </div>
@@ -17,8 +17,8 @@ export default {
             { message: 'One game board (scoring track)', image : '/img/logos/icone/icone_plateau.png'},
             { message: '68 cards ', image : '/img/logos/icone/icone_cartes.png'},
             { message: '6 Answer cards ', image : '/img/logos/icone/icone_carte_reponse.png' },
-            { message: '6 Answer pawns ', image : '/img/logos/icone/icone_plateau.png' },
-            { message: 'Main pawns ', image : '/img/logos/icone/icone_plateau.png' },
+            { message: '6 Answer pawns ', image : '/img/logos/icone/icone_pion_principal.png' },
+            { message: 'Main pawns ', image : '/img/logos/icone/icone_pion_reponse.png' },
 
             ]
         }
