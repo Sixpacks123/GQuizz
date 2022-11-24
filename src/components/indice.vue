e4c376cbf36c20a9c0ecb324875168d4a4bcd970
Heurtault Aubin <aub.heurtault@gmail.com>
2022-11-24 09:45:55 +0100

    clue

--- src/components/indice.vue
+++ src/components/indice.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <h3 class="font-extrabold">
-You know any art movement?</h3>
+        <h3 class="font-extrabold">Need a clue ?</h3>
         <div>
-            <p>On the <router-link to="/arts" class="text-jaune">page</router-link> arts movements you can find all the movements with a definition</p>
+            <p>On the  arts movements <router-link to="/arts" class="text-jaune">page</router-link> you can find all the movements with a definition</p>
         </div>
     </div>
 </template>
